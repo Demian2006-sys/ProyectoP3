5fae8fa8d499f0e9fde9568b020689c6603428aa
Demian2006-sys <demian.durand@udla.edu.ec>
2025-07-08 11:59:39 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "funciones.h"
 
 #define NUM_ZONAS 5
+
 int main() {
     Zona zonas[NUM_ZONAS];
     float limites[4] = {100, 75, 85, 25}; // CO2, SO2, NO2, PM2.5
@@ -22,76 +23,71 @@ int main() {
         printf("7. Guardar datos en archivo binario\n");
         printf("8. Cargar datos desde archivo binario\n");
         printf("9. Guardar monitoreo actual en archivo de texto\n");
+        printf("10. Actualizar histórico con monitoreo actual\n");
         printf("0. Salir\n");
         printf("Seleccione una opción: ");
         if (scanf("%d", &opcion) != 1) {
             printf("Entrada inválida. Intente de nuevo.\n");
-            while(getchar() != '\n'); // limpiar buffer
+            while(getchar() != '\n');
             continue;
         }
 
-switch (opcion) {
-    case 1:
-        generarDatosHistoricos(zonas, NUM_ZONAS);
-        datosGenerados = 1;
-        datosCargados = 0;
-        break;
-
-    case 2:
-        cargarDesdeArchivo("datos.txt", zonas, NUM_ZONAS);
-        datosCargados = 1;
-        datosGenerados = 0;
-        break;
-
-    case 3:
-        if (!datosDisponibles(datosGenerados, datosCargados)) break;
-        monitorearContaminacion(zonas, NUM_ZONAS, limites);
-        break;
-
-    case 4:
-        if (!datosDisponibles(datosGenerados, datosCargados)) break;
-        predecirContaminacion(zonas, NUM_ZONAS);
-        emitirAlertas(zonas, NUM_ZONAS, limites);
-        break;
-
-    case 5:
-        if (!datosDisponibles(datosGenerados, datosCargados)) break;
-        generarRecomendaciones(zonas, NUM_ZONAS);
-        printf("Recomendaciones generadas.\n");
-        break;
-
-    case 6:
-        if (!datosDisponibles(datosGenerados, datosCargados)) break;
-        exportarInforme("informe.txt", zonas, NUM_ZONAS);
-        break;
-
-    case 7:
-        if (!datosDisponibles(datosGenerados, datosCargados)) break;
-        guardarDatos("datos.bin", zonas, NUM_ZONAS);
-        break;
-
-    case 8: {
-        int cargados = cargarDatos("datos.bin", zonas, NUM_ZONAS);
-        if (cargados > 0) {
-            datosCargados = 1;
-            datosGenerados = 0;
+        switch (opcion) {
+            case 1:
+                generarDatosHistoricos(zonas, NUM_ZONAS);
+                datosGenerados = 1;
+                datosCargados = 0;
+                break;
+            case 2:
+                cargarDesdeArchivo("datos.txt", zonas, NUM_ZONAS);
+                datosCargados = 1;
+                datosGenerados = 0;
+                break;
+            case 3:
+                if (!datosDisponibles(datosGenerados, datosCargados)) break;
+                monitorearContaminacion(zonas, NUM_ZONAS, limites);
+                break;
+            case 4:
+                if (!datosDisponibles(datosGenerados, datosCargados)) break;
+                predecirContaminacion(zonas, NUM_ZONAS);
+                emitirAlertas(zonas, NUM_ZONAS, limites);
+                break;
+            case 5:
+                if (!datosDisponibles(datosGenerados, datosCargados)) break;
+                generarRecomendaciones(zonas, NUM_ZONAS);
+                printf("Recomendaciones generadas.\n");
+                break;
+            case 6:
+                if (!datosDisponibles(datosGenerados, datosCargados)) break;
+                exportarInforme("informe.txt", zonas, NUM_ZONAS);
+                break;
+            case 7:
+                if (!datosDisponibles(datosGenerados, datosCargados)) break;
+                guardarDatos("datos.bin", zonas, NUM_ZONAS);
+                break;
+            case 8: {
+                int cargados = cargarDatos("datos.bin", zonas, NUM_ZONAS);
+                if (cargados > 0) {
+                    datosCargados = 1;
+                    datosGenerados = 0;
+                }
+                break;
+            }
+            case 9:
+                if (!datosDisponibles(datosGenerados, datosCargados)) break;
+                guardarMonitoreo("monitoreo.txt", zonas, NUM_ZONAS);
+                break;
+            case 10:
+                if (!datosDisponibles(datosGenerados, datosCargados)) break;
+                actualizarHistoricoConMonitoreo(zonas, NUM_ZONAS);
+                break;
+            case 0:
+                printf("Saliendo...\n");
+                break;
+            default:
+                printf("Opción inválida. Intente de nuevo.\n");
+                break;
         }
-        break;
-    }
-
-    case 9:
-        if (!datosDisponibles(datosGenerados, datosCargados)) break;
-        guardarMonitoreo("monitoreo.txt", zonas, NUM_ZONAS);
-        break;
-
-    case 0:
-        printf("Saliendo...\n");
-        break;
-
-    default:
-        printf("Opción inválida. Intente de nuevo.\n");
-        break;
-}
     } while (opcion != 0);
 
     return 0;
